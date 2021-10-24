bd0240854f251940d42a940da82f3adb3b62c3a6
CameronWhitworth <cameronwhitworth@hotmail.com>
2021-10-25 00:17:55 +0100

    basic frame

---
+++ src/App.vue
@@ -0,0 +1,269 @@
+<template>
+  <div id="app">
+    <v-app>
+      <!-- <v-app-bar
+      app
+      color="white"
+      height="100"
+    >
+      <v-avatar
+        class="mr-3"
+        color="grey lighten-5"
+        size="70"
+      >
+        <v-img
+          contain
+          max-height="70%"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        ></v-img>
+      </v-avatar>
+
+      <v-toolbar-title class="font-weight-black headline">
+        VUETIFY
+      </v-toolbar-title>
+    </v-app-bar> -->
+
+      <v-content>
+        <section id="hero">
+          <v-row no-gutters>
+            <v-img
+              :min-height="'calc(100vh - ' + $vuetify.application.top + 'px)'"
+              src="./assets/hero_banner_3.jpg"
+            >
+              <v-theme-provider dark>
+                <v-container fill-height>
+                  <v-row
+                    align="center"
+                    class="white--text mx-auto"
+                    justify="center"
+                  >
+                    <v-col class="white--text text-center" cols="12" tag="h1">
+                      <span
+                        :class="[
+                          $vuetify.breakpoint.smAndDown
+                            ? 'display-1'
+                            : 'display-2',
+                        ]"
+                        class="font-weight-light"
+                      >
+                        CAMERON WHITWORTH
+                      </span>
+
+                      <br />
+
+                      <span
+                        :class="[
+                          $vuetify.breakpoint.smAndDown
+                            ? 'display-3'
+                            : 'display-4',
+                        ]"
+                        class="font-weight-black"
+                      >
+                        PORTFOLIO
+                      </span>
+                    </v-col>
+
+                    <v-btn
+                      class="align-self-end"
+                      fab
+                      outlined
+                      @click="$vuetify.goTo('#about-me')"
+                    >
+                      <v-icon>mdi-chevron-double-down</v-icon>
+                    </v-btn>
+                  </v-row>
+                </v-container>
+              </v-theme-provider>
+            </v-img>
+          </v-row>
+        </section>
+
+        <section id="about-me" class="grey lighten-2">
+          <div class="py-12"></div>
+
+          <v-container class="text-center">
+            <h2 class="display-2 font-weight-bold mb-3">ABOUT ME</h2>
+
+            <v-responsive class="mx-auto mb-8" width="56">
+              <v-divider class="mb-1"></v-divider>
+
+              <v-divider></v-divider>
+            </v-responsive>
+
+            <v-responsive
+              class="mx-auto title font-weight-light mb-8"
+              max-width="720"
+            >
+              I’m a Junior developer looking to expand my existing skills 
+              in creating user driven applications. I graduated from university 
+              with a First-Class Honours in computer science.
+            </v-responsive>
+
+            <v-avatar class="elevation-12 mb-12" size="128">
+              <v-img src="https://media-exp1.licdn.com/dms/image/C4E03AQEm9RbsmrNBRw/profile-displayphoto-shrink_200_200/0/1629892992315?e=1640822400&v=beta&t=RRGyzv2s_VQDQdBA20Q_dp9T3P0aMlCDOEsZex3aHAo"></v-img>
+            </v-avatar>
+
+            <div></div>
+            <div>
+            <v-btn class="ma-2" target="_blank" color="grey" href="https://www.linkedin.com/in/cameron-whitworth-997k/" outlined large>
+              <span class="grey--text text--darken-1 font-weight-bold">
+                LINKEDIN
+              </span>
+            </v-btn>
+            <v-btn class="ma-2" target="_blank" color="grey" href="https://github.com/CameronWhitworth" outlined large>
+              <span class="grey--text text--darken-1 font-weight-bold">
+                GITHUB
+              </span>
+            </v-btn>
+            </div>
+          </v-container>
+
+          <div class="py-12"></div>
+        </section>
+
+        <section id="projects" >
+          <div class="py-12"></div>
+
+          <v-container>
+            <h2
+              class="display-2 font-weight-bold mb-3 text-uppercase text-center"
+            >
+              Projects
+            </h2>
+
+            <v-responsive class="mx-auto mb-12" width="56">
+              <v-divider class="mb-1"></v-divider>
+
+              <v-divider></v-divider>
+            </v-responsive>
+
+            <v-row>
+              <v-col
+                v-for="({ src, text, title }, i) in projects"
+                :key="i"
+                cols="12"
+                md="4"
+              >
+                <v-img
+                  :src="src"
+                  class="mb-4"
+                  height="275"
+                  max-width="100%"
+                ></v-img>
+
+                <h3
+                  class="font-weight-black mb-4 text-uppercase"
+                  v-text="title"
+                ></h3>
+
+                <div class="title font-weight-light mb-5" v-text="text"></div>
+
+                <v-btn class="ml-n4 font-weight-black" target="_blank" text href="github">
+                  GITHUB
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-container>
+
+          <div class="py-12"></div>
+        </section>
+
+        <section id="features" class="grey lighten-3">
+          <div class="py-12"></div>
+
+          <v-container class="text-center">
+            <h2 class="display-2 font-weight-bold mb-3">Personal Skills</h2>
+
+            <v-responsive class="mx-auto mb-12" width="56">
+              <v-divider class="mb-1"></v-divider>
+
+              <v-divider></v-divider>
+            </v-responsive>
+
+            <v-row>
+              <v-col
+                v-for="({ icon, title, text }, i) in features"
+                :key="i"
+                cols="12"
+                md="4"
+              >
+                <v-card class="py-12 px-4" color="grey lighten-5" flat>
+                  <v-theme-provider dark>
+                    <div>
+                      <v-avatar color="primary" size="88">
+                        <v-icon large v-text="icon"></v-icon>
+                      </v-avatar>
+                    </div>
+                  </v-theme-provider>
+
+                  <v-card-title
+                    class="justify-center font-weight-black text-uppercase"
+                    v-text="title"
+                  ></v-card-title>
+
+                  <v-card-text class="subtitle-1" v-text="text"> </v-card-text>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-container>
+
+          <div class="py-12"></div>
+        </section>
+      </v-content>
+
+      <v-footer class="justify-center" color="#292929" height="100">
+        <div
+          class="title font-weight-light grey--text text--lighten-1 text-center"
+        >
+          &copy; {{ new Date().getFullYear() }} — Cameron Whitworth
+        </div>
+      </v-footer>
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          src: "https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+          title: "Flocking Algorithm ",
+          text: "A recreation of the Boids flocking algorithm designed by Craig Reynolds. Project was built in JavaScript using the P5.js library and has being optimized through Quadtree implementation.",
+          github: "https://github.com/CameronWhitworth/flockAI",
+        },
+        {
+          src: "https://images.unsplash.com/photo-1475938476802-32a7e851dad1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+          title: "Catalogue video games",
+          text: "Tool designed for allowing users to track videos games own/played. Was built with Vue.js and the Vuetify material liberty. Connects to “RAWGs” game database API to fetch data. Users catalogue is saved to firebase.",
+          github: "https://github.com/CameronWhitworth/flockAI",
+        },
+        {
+          src: "https://images.unsplash.com/photo-1416339442236-8ceb164046f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1892&q=80",
+          title: "Another Project",
+          text: "Find something to add here",
+          github: "https://github.com/CameronWhitworth/flockAI",
+        },
+      ],
+      features: [
+        {
+          icon: "mdi-account-group-outline",
+          title: "Team Player",
+          text: "Some text here",
+        },
+        {
+          icon: "mdi-pencil",
+          title: "Excited To Learn",
+          text: "Some text here",
+        },
+        {
+          icon: "mdi-widgets",
+          title: "User Driven",
+          text: "Some text here",
+        },
+      ],
+    };
+  },
+};
+</script>
